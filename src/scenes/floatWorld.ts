ef332fb7be7b9b3ed7fef5fa35d3d50889b8e559
alphardex <2582347430@qq.com>
2021-01-26 12:37:11 +0800

    update

--- src/scenes/floatWorld.ts
+++ src/scenes/floatWorld.ts
@@ -2,17 +2,24 @@ import * as THREE from "three";
 import ky from "kyouka";
 import { floatWorldFontConfig, floatWorldFontUrl } from "@/consts/floatWorld";
 import { Base } from "./base";
+import { CinematicCamera } from "three/examples/jsm/cameras/CinematicCamera.js";
+import { calcAspect } from "@/utils/math";
 
 class FloatWorld extends Base {
   meshes!: THREE.Mesh[];
+  radius!: number;
+  theta!: number;
   constructor(sel: string, debug: boolean) {
     super(sel, debug);
+    this.cameraPosition = new THREE.Vector3(2, 1, 15);
     this.meshes = [];
+    this.radius = 15;
+    this.theta = 0;
   }
   // 初始化
   async init() {
     this.createScene();
-    this.createPerspectiveCamera();
+    this.createCinematicCamera();
     this.createRenderer();
     await this.createFloatText();
     this.createFloatObjects();
@@ -20,6 +27,16 @@ class FloatWorld extends Base {
     this.createOrbitControls();
     this.addListeners();
     this.setLoop();
+  }
+  // 创建相机
+  createCinematicCamera() {
+    const { perspectiveCameraParams, cameraPosition, lookAtPosition } = this;
+    const { fov, near, far } = perspectiveCameraParams;
+    const aspect = calcAspect(this.container!);
+    const camera = new CinematicCamera(fov, aspect, near, far);
+    camera.position.copy(cameraPosition);
+    camera.lookAt(lookAtPosition);
+    this.camera = camera;
   }
   // 创建文字
   async createFloatText() {
@@ -29,7 +46,7 @@ class FloatWorld extends Base {
       ...floatWorldFontConfig,
     };
     const mesh = this.createText(
-      "Hello three.js",
+      "alphardex CSS Mahoutsukai",
       config,
       new THREE.MeshNormalMaterial()
     );
@@ -37,10 +54,14 @@ class FloatWorld extends Base {
     this.scene.add(mesh);
   }
   // 创建漂浮物
-  createFloatObjects(count = 100) {
-    const geometry = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45);
+  createFloatObjects(count = 160) {
+    const geometries = [
+      new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45),
+      new THREE.BoxBufferGeometry(0.5, 0.5, 0.5, 25, 25, 25),
+    ];
     const material = new THREE.MeshNormalMaterial();
     for (let i = 0; i < count; i++) {
+      const geometry = ky.sample(geometries);
       const donut = this.createMesh({
         geometry,
         material,
@@ -60,6 +81,16 @@ class FloatWorld extends Base {
       donut.rotation.set(rot.x, rot.y, 0);
       donut.scale.copy(scale);
     }
+  }
+  // 动画
+  update() {
+    const { camera, scene, radius } = this;
+    this.theta += 0.25;
+    camera.position.x = radius * Math.sin(THREE.MathUtils.degToRad(this.theta));
+    camera.position.y = radius * Math.sin(THREE.MathUtils.degToRad(this.theta));
+    camera.position.z = radius * Math.cos(THREE.MathUtils.degToRad(this.theta));
+    camera.lookAt(scene.position);
+    camera.updateMatrixWorld();
   }
 }
 
