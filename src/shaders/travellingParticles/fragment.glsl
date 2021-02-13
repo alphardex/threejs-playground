8d60c83c6a6cd44ac289a284421d2e5b0f6958d1
alphardex <2582347430@qq.com>
2021-02-13 22:47:34 +0800

    update

--- src/shaders/travellingParticles/fragment.glsl
+++ src/shaders/travellingParticles/fragment.glsl
@@ -7,7 +7,7 @@ float invert(float n){
 void main(){
     vec2 uv=vec2(gl_PointCoord.x,invert(gl_PointCoord.y));
     vec2 cUv=2.*uv-1.;
-    vec3 originColor=vec3(4./255.,10./255.,20./255.);
+    vec3 originColor=vec3(4.,9.8,12.6)/255.;
     vec4 color=vec4(.08/length(cUv));
     color*=vOpacity;
     color.rgb*=originColor*120.;
