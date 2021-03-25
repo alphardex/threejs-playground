7dbd9169ed0d72c757d91e3662859f3871be6ede
alphardex <2582347430@qq.com>
2021-03-25 11:05:41 +0800

    update

--- src/views/Sun.vue
+++ src/views/Sun.vue
@@ -10,7 +10,7 @@ export default defineComponent({
   name: "Sun",
   setup() {
     const start = () => {
-      const sun = new Sun(".sun", true);
+      const sun = new Sun(".sun", false);
       sun.init();
     };
     onMounted(() => {
