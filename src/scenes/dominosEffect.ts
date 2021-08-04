184ce1fa687c22a8c676dcbd37ad9d4b7c234665
alphardex <2582347430@qq.com>
2021-08-04 14:05:55 +0800

    update

--- src/scenes/dominosEffect.ts
+++ src/scenes/dominosEffect.ts
@@ -13,6 +13,7 @@ import {
 
 class DominosEffect extends PhysicsBase {
   clock!: THREE.Clock;
+  groundMat!: THREE.MeshPhongMaterial;
   cubeMat!: THREE.MeshPhongMaterial;
   sphereMat!: THREE.MeshPhongMaterial;
   ground!: MeshPhysicsObject;
@@ -23,6 +24,7 @@ class DominosEffect extends PhysicsBase {
     this.clock = new THREE.Clock();
     this.cameraPosition = new THREE.Vector3(0, 3, 5);
     this.params = {
+      groundColor: "#f0f0f0", // 地面颜色
       cubeColor: "#002f93", // 骨牌颜色
       sphereColor: "silver", // 球体颜色
     };
@@ -34,58 +36,40 @@ class DominosEffect extends PhysicsBase {
     this.createScene();
     this.createPerspectiveCamera();
     this.createRenderer();
-    this.createCubeMaterial();
+    this.changeRendererParams();
+    this.enableShadow();
+    this.createGroundMaterial();
     this.createGround();
+    this.createCubeMaterial();
     this.createCube({});
     this.createCubes();
     this.createSphereMaterial();
     this.createBall();
     this.applyForce2Ball();
-    this.hideSth();
     this.createLight();
     this.createOrbitControls();
     this.addListeners();
     this.setLoop();
   }
-  // 创建骨牌材质
-  createCubeMaterial() {
-    const { cubeColor } = this.params;
-    const cubeMat = new THREE.MeshPhongMaterial({
-      color: new THREE.Color(cubeColor),
+  // 创建地面材质
+  createGroundMaterial() {
+    const groundColor = this.params.groundColor;
+    const groundMat = new THREE.MeshPhongMaterial({
+      color: new THREE.Color(groundColor),
     });
-    this.cubeMat = cubeMat;
-  }
-  // 创建骨牌
-  createCube({ position = new Point({ x: 0, y: 0, z: 0 }) }) {
-    const box = new Point({ x: 0.1, y: 1, z: 0.5 });
-    const boxHalf = new Point({ x: box.x / 2, y: box.y / 2, z: box.z / 2 });
-    const geo = new THREE.BoxBufferGeometry(box.x, box.y, box.z);
-    const mat = this.cubeMat;
-    const mesh = new THREE.Mesh(geo, mat);
-    mesh.position.copy(point2ThreeVector(position));
-    this.scene.add(mesh);
-
-    const body = new CANNON.Body({
-      mass: 1,
-      shape: new CANNON.Box(point2CannonVec(boxHalf)),
-      position: point2CannonVec(position),
-    });
-    this.world.addBody(body);
-
-    const obj = new MeshPhysicsObject(mesh, body);
-    this.meshPhysicsObjs.push(obj);
-    return obj;
+    this.groundMat = groundMat;
   }
   // 创建地面
   createGround() {
     const position = new Point({ x: 0, y: -0.5, z: 0 });
     const rotation = new Point({ x: ky.deg2rad(-90), y: 0, z: 0 });
     const geo = new THREE.PlaneBufferGeometry(100, 100);
-    const mat = this.cubeMat;
+    const mat = this.groundMat;
     const mesh = new THREE.Mesh(geo, mat);
     mesh.position.copy(point2ThreeVector(position));
     // @ts-ignore
     mesh.rotation.set(...point2Array(rotation));
+    mesh.receiveShadow = true;
     this.scene.add(mesh);
 
     const body = new CANNON.Body({
@@ -103,6 +87,37 @@ class DominosEffect extends PhysicsBase {
     this.ground = obj;
     return obj;
   }
+  // 创建骨牌材质
+  createCubeMaterial() {
+    const cubeColor = this.params.cubeColor;
+    const cubeMat = new THREE.MeshPhongMaterial({
+      color: new THREE.Color(cubeColor),
+    });
+    this.cubeMat = cubeMat;
+  }
+  // 创建骨牌
+  createCube({ position = new Point({ x: 0, y: 0, z: 0 }) }) {
+    const box = new Point({ x: 0.1, y: 1, z: 0.5 });
+    const boxHalf = new Point({ x: box.x / 2, y: box.y / 2, z: box.z / 2 });
+    const geo = new THREE.BoxBufferGeometry(box.x, box.y, box.z);
+    const mat = this.cubeMat;
+    const mesh = new THREE.Mesh(geo, mat);
+    mesh.position.copy(point2ThreeVector(position));
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+    this.scene.add(mesh);
+
+    const body = new CANNON.Body({
+      mass: 1,
+      shape: new CANNON.Box(point2CannonVec(boxHalf)),
+      position: point2CannonVec(position),
+    });
+    this.world.addBody(body);
+
+    const obj = new MeshPhysicsObject(mesh, body);
+    this.meshPhysicsObjs.push(obj);
+    return obj;
+  }
   // 创建多个骨牌
   createCubes() {
     const pairCount = 10;
@@ -116,7 +131,7 @@ class DominosEffect extends PhysicsBase {
   }
   // 创建球体材质
   createSphereMaterial() {
-    const { sphereColor } = this.params;
+    const sphereColor = this.params.sphereColor;
     const sphereMat = new THREE.MeshPhongMaterial({
       color: new THREE.Color(sphereColor),
     });
@@ -128,6 +143,8 @@ class DominosEffect extends PhysicsBase {
     const mat = this.sphereMat;
     const mesh = new THREE.Mesh(geo, mat);
     mesh.position.copy(point2ThreeVector(position));
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
     this.scene.add(mesh);
 
     const body = new CANNON.Body({
@@ -163,9 +180,27 @@ class DominosEffect extends PhysicsBase {
     world.quatNormalizeFast = true;
     world.quatNormalizeSkip = 8;
   }
-  // 隐藏掉部分东西
-  hideSth() {
-    this.ground.mesh.visible = false;
+  // 更改渲染器参数
+  changeRendererParams() {
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+    this.renderer.toneMapping = THREE.CineonToneMapping;
+    this.renderer.toneMappingExposure = 1.5;
+  }
+  // 创建光源
+  createLight() {
+    // 环境光
+    const ambiLight = new THREE.AmbientLight(new THREE.Color("white"), 0.5);
+    this.scene.add(ambiLight);
+
+    // 聚光
+    const spotLight = new THREE.SpotLight(new THREE.Color("white"));
+    spotLight.position.set(15, 10, 10);
+    spotLight.castShadow = true;
+    spotLight.shadow.mapSize.width = 1024;
+    spotLight.shadow.mapSize.height = 1024;
+    spotLight.angle = 0.3;
+    spotLight.penumbra = 1;
+    this.scene.add(spotLight);
   }
 }
 
