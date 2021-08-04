c0741b43258e03ef0560c2364f9e7ee890c0d2a3
alphardex <2582347430@qq.com>
2021-08-04 10:12:39 +0800

    update

--- src/scenes/dominosEffect.ts
+++ src/scenes/dominosEffect.ts
@@ -1,70 +1,96 @@
 import * as THREE from "three";
 import ky from "kyouka";
-import * as dat from "dat.gui";
-import { Base } from "./base";
-// @ts-ignore
-import dominosEffectVertexShader from "../shaders/dominosEffect/vertex.glsl";
-// @ts-ignore
-import dominosEffectFragmentShader from "../shaders/dominosEffect/fragment.glsl";
+import { PhysicsBase } from "./base";
+import * as CANNON from "cannon-es";
+import { MeshPhysicsObject } from "@/utils/physics";
+import {
+  arrays2Point,
+  Point,
+  point2Array,
+  point2CannonVec,
+  point2ThreeVector,
+} from "@/utils/math";
 
-class DominosEffect extends Base {
+class DominosEffect extends PhysicsBase {
   clock!: THREE.Clock;
-  dominosEffectMaterial!: THREE.ShaderMaterial;
+  cubeMat!: THREE.MeshPhongMaterial;
+  params!: any;
   constructor(sel: string, debug: boolean) {
     super(sel, debug);
     this.clock = new THREE.Clock();
-    this.cameraPosition = new THREE.Vector3(0, 0, 1);
+    this.cameraPosition = new THREE.Vector3(0, 3, 5);
+    this.params = {
+      cubeColor: "#002f93", // 骨牌颜色
+    };
   }
   // 初始化
   init() {
+    this.createWorld();
     this.createScene();
     this.createPerspectiveCamera();
     this.createRenderer();
-    this.createDominosEffectMaterial();
-    this.createPlane();
+    this.createCubeMaterial();
+    this.createCube({ position: { x: 0, y: 0.5, z: 0 } });
+    this.createGround();
     this.createLight();
-    this.trackMousePos();
     this.createOrbitControls();
     this.addListeners();
     this.setLoop();
   }
-  // 创建材质
-  createDominosEffectMaterial() {
-    const dominosEffectMaterial = new THREE.ShaderMaterial({
-      vertexShader: dominosEffectVertexShader,
-      fragmentShader: dominosEffectFragmentShader,
-      side: THREE.DoubleSide,
-      uniforms: {
-        uTime: {
-          value: 0,
-        },
-        uMouse: {
-          value: new THREE.Vector2(0, 0),
-        },
-        uResolution: {
-          value: new THREE.Vector2(window.innerWidth, window.innerHeight),
-        },
-      },
+  // 创建骨牌材质
+  createCubeMaterial() {
+    const { cubeColor } = this.params;
+    const cubeMat = new THREE.MeshPhongMaterial({
+      color: new THREE.Color(cubeColor),
     });
-    this.dominosEffectMaterial = dominosEffectMaterial;
+    this.cubeMat = cubeMat;
   }
-  // 创建平面
-  createPlane() {
-    const geometry = new THREE.PlaneBufferGeometry(1, 1, 100, 100);
-    const material = this.dominosEffectMaterial;
-    this.createMesh({
-      geometry,
-      material,
+  // 创建骨牌
+  createCube({ position = new Point({ x: 0, y: 0, z: 0 }) }) {
+    const box = new Point({ x: 0.1, y: 1, z: 0.5 });
+    const boxHalf = new Point({ x: box.x / 2, y: box.y / 2, z: box.z / 2 });
+    const geo = new THREE.BoxBufferGeometry(box.x, box.y, box.z);
+    const mat = this.cubeMat;
+    const mesh = new THREE.Mesh(geo, mat);
+    mesh.position.copy(point2ThreeVector(position));
+    this.scene.add(mesh);
+
+    const body = new CANNON.Body({
+      mass: 1,
+      shape: new CANNON.Box(point2CannonVec(boxHalf)),
+      position: point2CannonVec(position),
     });
+    this.world.addBody(body);
+
+    const obj = new MeshPhysicsObject(mesh, body);
+    this.meshPhysicsObjs.push(obj);
+    return obj;
   }
-  // 动画
-  update() {
-    const elapsedTime = this.clock.getElapsedTime();
-    const mousePos = this.mousePos;
-    if (this.dominosEffectMaterial) {
-      this.dominosEffectMaterial.uniforms.uTime.value = elapsedTime;
-      this.dominosEffectMaterial.uniforms.uMouse.value = mousePos;
-    }
+  // 创建地面
+  createGround() {
+    const position = new Point({ x: 0, y: 0, z: 0 });
+    const rotation = new Point({ x: ky.deg2rad(-90), y: 0, z: 0 });
+    const geo = new THREE.PlaneBufferGeometry(100, 100);
+    const mat = this.cubeMat;
+    const mesh = new THREE.Mesh(geo, mat);
+    mesh.position.copy(point2ThreeVector(position));
+    // @ts-ignore
+    mesh.rotation.set(...point2Array(rotation));
+    this.scene.add(mesh);
+
+    const body = new CANNON.Body({
+      shape: new CANNON.Plane(),
+      position: point2CannonVec(position),
+      quaternion: new CANNON.Quaternion().setFromEuler(
+        // @ts-ignore
+        ...point2Array(rotation)
+      ),
+    });
+    this.world.addBody(body);
+
+    const obj = new MeshPhysicsObject(mesh, body);
+    this.meshPhysicsObjs.push(obj);
+    return obj;
   }
 }
 
