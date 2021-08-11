4f42ca01b726d7cf7764b2f3ae076a09785498bc
alphardex <2582347430@qq.com>
2021-08-11 19:21:16 +0800

    update

--- src/consts/noiseMarble.ts
+++ src/consts/noiseMarble.ts
@@ -1,8 +1,8 @@
 // https://polyhaven.com/
 const hdrUrl = "./static/hdrs/orbita.hdr";
 
-// const heightMapUrl = "./static/noisemaps/1.jpeg";
-// const displacementMapUrl = "./static/noisemaps/5.jpeg";
+const heightMapUrl = "./static/noisemaps/1.jpeg";
+const displacementMapUrl = "./static/noisemaps/5.jpeg";
 
 // http://kitfox.com/projects/perlinNoiseMaker/
 // const heightMapUrl = "./static/noisemaps/2.png";
@@ -17,7 +17,7 @@ const hdrUrl = "./static/hdrs/orbita.hdr";
 // const heightMapUrl = "./static/noisemaps/8.png";
 // const displacementMapUrl = "./static/noisemaps/5.jpeg";
 
-const heightMapUrl = "./static/noisemaps/9.png";
-const displacementMapUrl = "./static/noisemaps/5.jpeg";
+// const heightMapUrl = "./static/noisemaps/9.png";
+// const displacementMapUrl = "./static/noisemaps/5.jpeg";
 
 export { hdrUrl, heightMapUrl, displacementMapUrl };
