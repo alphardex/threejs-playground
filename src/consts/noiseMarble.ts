8d493caeccce46a838c27797e2e5d2dc6745ff5c
alphardex <2582347430@qq.com>
2021-08-11 09:40:54 +0800

    update

--- src/consts/noiseMarble.ts
+++ src/consts/noiseMarble.ts
@@ -4,10 +4,13 @@ const hdrUrl = "./static/hdrs/warehouse.hdr";
 // const heightMapUrl = "./static/noisemaps/2.png";
 // const displacementMapUrl = "./static/noisemaps/4.png";
 
-// const heightMapUrl = "./static/noisemaps/1.jpeg";
+const heightMapUrl = "./static/noisemaps/1.jpeg";
+const displacementMapUrl = "./static/noisemaps/5.jpeg";
+
+// const heightMapUrl = "./static/noisemaps/6.png";
 // const displacementMapUrl = "./static/noisemaps/5.jpeg";
 
-const heightMapUrl = "./static/noisemaps/6.png";
-const displacementMapUrl = "./static/noisemaps/5.jpeg";
+// const heightMapUrl = "./static/noisemaps/7.png";
+// const displacementMapUrl = "./static/noisemaps/5.jpeg";
 
 export { hdrUrl, heightMapUrl, displacementMapUrl };
