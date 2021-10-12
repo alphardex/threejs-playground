e1a3b2e22d69528d7f551bed9df41e24b91bb6e5
alphardex <2582347430@qq.com>
2021-10-12 15:56:18 +0800

    update

--- src/shaders/unrollImages/main/vertex.glsl
+++ src/shaders/unrollImages/main/vertex.glsl
@@ -10,21 +10,30 @@ varying vec3 vPosition;
 vec3 unroll(vec3 p,float angle,float progress){
     float rad=.1;
     float rolls=4.;
+    vec3 zAxis=vec3(0.,0.,1.);
+    vec3 d=vec3(-.5,.5,0.);
+    vec3 d2=vec3(-.5,.5,rad);
+    float sc=sin(angle)+cos(angle);
     
-    p=rotate(p-vec3(-.5,.5,0.),vec3(0.,0.,1.),-angle)+vec3(-.5,.5,0.);
+    p-=d;
+    p=rotate(p,zAxis,-angle);
+    p+=d;
     
-    float offset=(p.x+.5)/(sin(angle)+cos(angle));
+    float offset=(p.x+.5)/sc;
     float finalProgress=clamp((progress-offset*.99)/.01,0.,1.);
     
     p.z=rad+rad*(1.-offset/2.)*sin(-offset*rolls*PI-.5*PI);
     p.x=-.5+rad*(1.-offset/2.)*cos(-offset*rolls*PI+.5*PI);
     
-    p=rotate(p-vec3(-.5,.5,0.),vec3(0.,0.,1.),angle)+vec3(-.5,.5,0.);
+    p-=d;
+    p=rotate(p,zAxis,angle);
+    p+=d;
     
-    p=rotate(p-vec3(-.5,.5,rad),vec3(sin(angle),cos(angle),0.),-PI*progress*rolls);
+    p-=d2;
+    p=rotate(p,vec3(sin(angle),cos(angle),0.),-PI*progress*rolls);
     p+=vec3(
-        -.5+progress*cos(angle)*(sin(angle)+cos(angle)),
-        .5-progress*sin(angle)*(sin(angle)+cos(angle)),
+        -.5+progress*cos(angle)*sc,
+        .5-progress*sin(angle)*sc,
         rad*(1.-progress/2.)
     );
     
