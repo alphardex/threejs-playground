66da432858757ca42652554e4279e5e6edff1fc0
alphardex <2582347430@qq.com>
2021-10-16 21:12:09 +0800

    update

--- src/utils/physics.ts
+++ src/utils/physics.ts
@@ -6,7 +6,12 @@ class MeshPhysicsObject {
   body!: CANNON.Body;
   copyPosition!: boolean;
   copyQuaternion!: boolean;
-  constructor(mesh: THREE.Mesh | THREE.Object3D, body: CANNON.Body, copyPosition = true, copyQuaternion = true) {
+  constructor(
+    mesh: THREE.Mesh | THREE.Object3D,
+    body: CANNON.Body,
+    copyPosition = true,
+    copyQuaternion = true
+  ) {
     this.mesh = mesh;
     this.body = body;
     this.copyPosition = copyPosition;
